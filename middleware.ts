ba8ce3e2f899694a6ebbd7850f4ee1c6713d0695
Hilo22 <hieup0706@gmail.com>
2026-03-26 00:28:33 +0700

    fix: add matcher config to middleware

--- middleware.ts
+++ middleware.ts
@@ -53,6 +53,20 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|public).*)',
+  ],
+};
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
